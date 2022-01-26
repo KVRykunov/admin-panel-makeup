0f05806d05387553de3c5110cca787bea3169fc8
KVRykunov <kup42010@yandex.ru>
2022-01-26 21:26:41 +0700

    install husky

---
+++ index.js
@@ -0,0 +1,28 @@
+let lext = "net";
+if (5>3) {
+    text="da"
+}
+
+alert ("5 more thet 3? + text "da"")
+
+
+const tetrad = {
+     name: "tetrad",
+     weight: "300gr",
+     pages: "100lisov",
+     Oblojka: "solid",
+     color : {
+         face: "blue",
+         inside: "yellow"
+     }
+}
+if (tetrad.open) {
+    tetrad.size = "150cm"
+} else {
+    tetrad.size = "75cm"
+}
+
+
+if (b) {
+    Object.2=(2)}
+else (a)
